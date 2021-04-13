86798432d22f70a25fda92238e7241c871023869
gusbikos <gusbikos@gmail.com>
2021-04-13 12:28:55 -0400

    Completed

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,41 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listings, onDelete }) {
+  const [sorted, setSorted] = useState(false)
+
+  const allListings = listings.map((listing) => {
+    return (
+      <ListingCard
+        key={listing.id}
+        listing={listing}   
+        onDelete={onDelete}
+      />  
+    ) 
+  })
+
+  const sortedArray = [...listings]
+  sortedArray.sort(function(a, b) {
+    return a.location.localeCompare(b.location)
+  })
+
+  const mapArray = sortedArray.map((list) => (
+    <ListingCard
+        key={list.id}
+        listing={list}
+        onDelete={onDelete}
+      />  
+  ))
+
+  function handleSort() {
+    setSorted(sorted => !sorted)
+  }
+
   return (
     <main>
+      <button onClick={handleSort}>sorted</button>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {sorted ? mapArray : allListings}
       </ul>
     </main>
   );
